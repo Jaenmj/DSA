8144fd5c97f88462c04a947c9d8146c3f472d8a3
Jaenmj <165581270+Jaenmj@users.noreply.github.com>
2024-05-31 12:29:35 +0200

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,33 @@
+let mapContainer = document.getElementById("map-container");
+let buttonsContainer = document.getElementById("buttons-container");
+let backButton = document.getElementById("back-button");
+
+function showMap(location) {
+  buttonsContainer.style.display = "none";
+  backButton.style.display = "block";
+  switch (location) {
+    case "mecanicos":
+      mapContainer.innerHTML="hospi"
+      break;
+    case "hospitales":
+      mapContainer.innerHTML
+      break;
+    case "comisarias":
+      mapContainer.innerHTML
+      break;
+    default:
+      mapContainer.innerHTML
+  }
+}
+
+function goBack() {
+  buttonsContainer.style.display = "flex";
+  backButton.style.display = "none";
+  mapContainer.innerHTML = "";
+}
+
+function goBack() {
+  buttonsContainer.style.display = "flex";
+  backButton.style.display = "none";
+  mapContainer.innerHTML = "";
+}
