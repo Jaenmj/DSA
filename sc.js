54e5ed46ff08c8f5053b60f037e66ab06c4e5d42
Jaenmj <165581270+Jaenmj@users.noreply.github.com>
2024-06-04 17:22:11 +0200

    Add files via upload

--- sc.js
+++ sc.js
@@ -9,12 +9,19 @@ function showMap(section) {
   
     document.getElementById('buttons-container').style.display = 'none';
     document.getElementById('back-button').style.display = 'block';
+    document.getElementById('info-container').style.display = 'none';
+    document.getElementById('info-2').style.display = 'none';
+    document.getElementById('info-1').style.display = 'none';
+
   }
   
   function goBack() {
     document.getElementById('buttons-container').style.display = 'block';
     document.getElementById('back-button').style.display = 'none';
-  
+    document.getElementById('info-container').style.display = 'block';
+    document.getElementById('info-2').style.display = 'block';
+    document.getElementById('info-1').style.display = 'block';
+
     const sections = document.querySelectorAll('#map-container .section');
     sections.forEach(sec => sec.style.display = 'none');
   }
